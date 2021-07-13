adb089f2dfd3881a8c027935f25ffa747e2f71a0
ng-designs <nafis.gimranov@gmail.com>
2021-07-13 22:41:28 +0500

    solved subtask 2

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -3,10 +3,10 @@ package subtask2
 class SquareDecomposer {
 
     fun decomposeNumber(number: Int): Array<Int>? {
-        return getSquares(number)
+        return GetNextSquare(number)
     }
 
-    private fun getSquares(number: Int, numSqr: Long = Sqr(number.toLong())): Array<Int>? {
+    private fun GetNextSquare(number: Int, numSqr: Long = Sqr(number.toLong())): Array<Int>? {
         var resArray:Array<Int>? = arrayOf()
         var remain: Long
         for (i in number - 1 downTo 1) {
@@ -17,7 +17,7 @@ class SquareDecomposer {
 
             remain = numSqr - Sqr(i.toLong())
             if (remain > 0) {
-                resArray = getSquares(i, remain)
+                resArray = GetNextSquare(i, remain)
                 if (resArray != null) {
                     resArray += i
                     return resArray
