caa73de172f4aa85dcf6514b275646d053226d51
ng-designs <nafis.gimranov@gmail.com>
2021-07-14 21:37:41 +0500

    solved subtask 1

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -4,6 +4,18 @@ class Combinator {
 
     // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        for (i in 1..array[1]) {
+            if (array[0] <= array[1].getFactorial() / ((array[1] - i).getFactorial() * i.getFactorial()))
+                return i
+        }
+        return null
+    }
+
+    private fun Int.getFactorial(): Long {
+        var value:Long = 1L
+        for (i in 2..this) {
+            value *= i
+        }
+        return value
     }
 }
